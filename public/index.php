0678db95a5e5518faa88a85d65f7d92ebde73afd
Larry Garfield <larry@garfieldtech.com>
2026-02-15 13:45:26 -0600

    Move most of index.php into MiDy, for easier maintenance.

--- public/index.php
+++ public/index.php
@@ -2,23 +2,8 @@
 
 declare(strict_types=1);
 
-use HttpSoft\Emitter\SapiEmitter;
-use Nyholm\Psr7Server\ServerRequestCreator;
-use Symfony\Component\Dotenv\Dotenv;
 use App\App;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = new Dotenv();
-$envFile = __DIR__ . '/../.env';
-if (file_exists($envFile)) {
-    $dotenv->loadEnv($envFile);
-}
-
-$app = new App();
-
-$serverRequest = $app->container->get(ServerRequestCreator::class)->fromGlobals();
-
-$response = $app->handle($serverRequest);
-
-$app->container->get(SapiEmitter::class)->emit($response);
+new App()->run();
